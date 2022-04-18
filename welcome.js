cb81f2ba2099c42316a6210e36b77f909b9d1333
Nizam <nizam.oezdemir@gmx.ch>
2022-04-18 20:15:35 +0200

    href Link angepasst für content.html

--- welcome.js
+++ welcome.js
@@ -13,45 +13,10 @@ setTimeout(function() {
             $('.fade-in-text-1').fadeIn("3000");
             setTimeout(function() { 
                 $('.fade-in-text-1').fadeOut("3000");
-                window.location.href = "../content.html"; 
+                window.location.href = "../STUMTEC/content.html"; 
             }, 3000);
         }, 2000);
     }); 
 }, 4000);
-/*
-$(document).ready(function() {
-    $('.fade-in-text-1').fadeOut("slow");
-    setTimeout(() => {  
-        $('.fade-in-text-1').css("display", "none");
-        $('.fade-in-text-2').fadeIn("4000");
-    }, 2000);
-}); 
-
-/*
-$(function() {  
-    setTimeout(function() {
-        $('.fade-in-text-1').removeClass('hidden');
-    }, 500); 
-});
-
-setTimeout(function() { 
-    $(document).ready(function() {
-        $(".fade-in-text-1").fadeIn();
-    }); 
-}, 3000);
-
-setTimeout(function() { 
-    $(document).ready(function() {
-        $(".fade-in-text-1").fadeOut();
-    }); 
-}, 3000);
-
-
-
-setTimeout(function() { 
-    window.location.href = "../index2.html"; 
-}, 5000);
-*/
-
 
 //setTimeout(() => {  console.log("World!"); }, 2000);
