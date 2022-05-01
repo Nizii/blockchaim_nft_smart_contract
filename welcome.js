b8757fda38eece3e8a4e21d76f15767bc3703bb1
Nizam <nizam.oezdemir@gmx.ch>
2022-05-01 12:38:19 +0200

    Startsequenz angepasst "Marcs Code auskommentiert"

--- welcome.js
+++ welcome.js
@@ -1,28 +1,41 @@
 $(function() {  
     setTimeout(function() {
-        $('.fly-in-text').removeClass('hidden');
+        $('.fade-in-text-1').removeClass('hidden');
     }, 500); 
 });
 
-var ethConnectPromise = window.ethereum.request({ method: 'eth_requestAccounts' });
+//Code von Marc 
+//var ethConnectPromise = window.ethereum.request({ method: 'eth_requestAccounts' });
 
 setTimeout(function() { 
     $(document).ready(function() {
-        $("ul").fadeOut("slow");
-        $('.fade-in-text-1').removeClass('hidden');
+        $('.fade-in-text-1').fadeIn("slow");
         setTimeout(() => {  
-            $("ul").css("display", "none");
-            Promise.resolve(ethConnectPromise).then(function(){
-                window.location.href = "./content.html";
-                console.log("proceed to content");
-              }).catch(function(error)
-              {
-                $('.fade-in-text-1').fadeIn("3000");
-                console.log("you must connect your ethereum wallet before continueing to this site!")
-                document.getElementById("connectText").innerHTML = "There was an Error with connecting your Ethereum Wallet. Please try again by reloading the page to continue browsing this site."
-              })
+            $('.fade-in-text-1').fadeOut("slow");
+            setTimeout(() => {
+                $('.fade-in-text-2').removeClass('hidden');
+                $('.fade-in-text-2').fadeIn();
+                /* Von hier bis... */
+                setTimeout(() => {
+                    window.location.href = "../content.html";
+                }, 4000);
+                /* bis hier... kann gelöst werden*/
+
+                /* Code von Marc 
+                Promise.resolve(ethConnectPromise).then(function(){
+                    window.location.href = "../content.html";
+                    console.log("proceed to content");
+                }).catch(function(error)
+                {
+                    $('.fade-in-text-1').fadeIn("3000");
+                    console.log("you must connect your ethereum wallet before continueing to this site!")
+                    document.getElementById("connectText").innerHTML = "There was an Error with connecting your Ethereum Wallet. Please try again by reloading the page to continue browsing this site."
+                })
+                */    
+
+            }, 2000);
         }, 2000);
     }); 
-}, 4000);
+}, 2000);
 
 //setTimeout(() => {  console.log("World!"); }, 2000);
